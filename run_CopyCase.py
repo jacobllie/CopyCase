f9df4c1581e37babaae2dc860a4766ad6eae8fda
jacobllie <72126694+jacobllie@users.noreply.github.com>
2024-08-15 10:55:18 +0200

    path

--- run_CopyCase.py
+++ run_CopyCase.py
@@ -4,10 +4,10 @@ import os
 
 # Kjører copycase
 
-PATH = "I:\\STOLAV - Kreftklinikken\\Avdelinger kreft\\Avdeling for stråleterapi\\Fysikere\\Kliniske skript\\raystation-scripts" \
-       "\\resources\\CopyCase"
+#PATH = "I:\\STOLAV - Kreftklinikken\\Avdelinger kreft\\Avdeling for stråleterapi\\Fysikere\\Kliniske skript\\raystation-scripts" \
+#       "\\resources\\CopyCase"
 
-#PATH = "H:/Dokumenter/Github/CopyCase"
+PATH = "H:/Dokumenter/Github/CopyCase"
 sys.path.append(PATH)
 
 import CopyCase as copycase
